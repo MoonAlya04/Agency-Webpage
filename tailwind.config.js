c7fe14a79407fa2ba2ca07ac65172709b7d14cdb
MoonAlya04 <alyamoonbaek@gmail.com>
2025-05-25 14:57:41 +0400

    Work in progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,24 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+
+}
+
+module.exports = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#60E1CB',
+        dark: '#292525',
+        accent: '#7BC0E3',
+
+        // Dark mode colors
+        'primary-dark': '#3FB9A7',
+        'dark-dark': '#1C1C1C',
+        'accent-dark': '#5AAED2',
+      },
+    },
+  },
+  plugins: [],
 }
